92d1c2d864b2f5db0296b9c6bf5b603de6c9b82b
Krishna Sahoo <krishnasahoo11156@gmail.com>
2026-05-06 20:14:15 +0530

    Add Table.java - multiplication table program

--- Basics/Table.java
+++ Basics/Table.java
@@ -2,22 +2,14 @@ package Basics;
 
 import java.util.Scanner;
 
-public class EvenOdd {
+public class Table {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        // Taking input from user
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
-
-        // Checking even or odd
-        if (num % 2 == 0) {
-            System.out.println(num + " is Even");
-        } else {
-            System.out.println(num + " is Odd");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
         }
-
         sc.close();
     }
 }
