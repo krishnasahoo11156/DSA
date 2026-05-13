70c8dfcc900d4b8d1b47681ead8b6f4e13e60788
Krishna <krishnasahoo11156@gmail.com>
2026-05-13 19:41:12 +0530

    feat: add program to find Armstrong numbers within a specified range

--- first-java/Armstrong.java
+++ first-java/Armstrong.java
@@ -1,5 +1,44 @@
 // To find Armstrong Number between two given number.
 
+import java.util.Scanner;
+
 public class Armstrong {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+
+        System.out.println("Armstrong numbers between " + start + " and " + end + " are:");
+
+        for (int num = start; num <= end; num++) {
+
+            int original = num;
+            int temp = num;
+
+            // Count digits
+            int digits = String.valueOf(num).length();
+
+            int sum = 0;
+
+            // Calculate Armstrong sum
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += Math.pow(digit, digits);
+                temp /= 10;
+            }
+
+            // Check Armstrong
+            if (sum == original) {
+                System.out.println(original);
+            }
+        }
+
+        sc.close();
+    }
 }
